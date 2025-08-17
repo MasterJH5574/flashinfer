dc813cdbc78d17f6a131e5d8d061b9cd0b38fd7d
Ruihang Lai <ruihangl@cs.cmu.edu>
2025-08-16 23:27:46 -0400

    BatchPagedAttentionPlan pass in num_sm

--- include/flashinfer/attention/scheduler.cuh
+++ include/flashinfer/attention/scheduler.cuh
@@ -1088,23 +1088,22 @@ inline cudaError_t TwoStageHolisticPlan(void* float_buffer, size_t float_workspa
                                         IdType* kv_indptr_h, IdType* kv_len_arr_h,
                                         uint32_t batch_size, uint32_t num_qo_heads,
                                         uint32_t num_kv_heads, uint32_t head_dim, bool causal,
-                                        cudaStream_t stream) {
+                                        cudaStream_t stream, int* num_sm) {
   constexpr uint32_t NUM_TASKS = 2;
   const uint32_t CTA_TILE_Q_SIZES[NUM_TASKS] = {128, 16};
-  int num_sm = 0;
   int dev_id = 0;
 
   uint32_t gqa_group_size = num_qo_heads / num_kv_heads;
   FLASHINFER_CUDA_CALL(cudaGetDevice(&dev_id));
-  FLASHINFER_CUDA_CALL(cudaDeviceGetAttribute(&num_sm, cudaDevAttrMultiProcessorCount, dev_id));
+  FLASHINFER_CUDA_CALL(cudaDeviceGetAttribute(num_sm, cudaDevAttrMultiProcessorCount, dev_id));
 
   if (head_dim >= 256) {
     // NOTE (Yilong): optimize this code path
     // constraint gridDim due to cooperative group
-    num_sm *= 1;
+    *num_sm *= 1;
   } else {
     // NOTE(Zihao): two cta per sm
-    num_sm *= 2;
+    *num_sm *= 2;
   }
 
   // step 0. determine the number of blocks in x and y dimensions
@@ -1129,7 +1128,7 @@ inline cudaError_t TwoStageHolisticPlan(void* float_buffer, size_t float_workspa
   }
 
   int cluster_size = 1;
-  int num_clusters = num_sm / cluster_size;
+  int num_clusters = *num_sm / cluster_size;
   plan_info.num_blks_x = cluster_size;
   plan_info.num_blks_y = num_clusters;
 
